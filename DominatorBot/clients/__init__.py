869607a57b85684f78f73946def78beca65c7308
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-08-26 09:25:48 -0700

    Update __init__.py

--- DominatorBot/clients/__init__.py
+++ DominatorBot/clients/__init__.py
@@ -1,3 +1,3 @@
 from .client_list import clients_list, client_id
-from .decs import hell_cmd, hell_handler
-from .session import Hell, H2, H3, H4, H5, DominatorBot
+from .decs import dominator_cmd, dominator_handler
+from .session import Dominator, H2, H3, H4, H5, DominatorBot
