23fb7420e5101ece52ea42a132f3412bde7d79e1
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-08-28 02:26:02 -0700

    Delete cyberlegend.py

--- DominatorBot/plugins/cyberlegend.py
+++
@@ -1,9 +0,0 @@
-
-from i legend import legendbot 
-from legendbot import cyber await()
-try()legendbot 
-                   await = legend=("sreing")
-  
-                                        legend("proram)loop_sesssion[pro].string
-                                               legend=[]
-                                               
