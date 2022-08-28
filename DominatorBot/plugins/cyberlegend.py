ee102b87c5e16c5f2bb67b4ae6d52e6eb71324e3
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-08-28 02:25:54 -0700

    Update and rename o to cyberlegend.py

---
+++ DominatorBot/plugins/cyberlegend.py
@@ -0,0 +1,9 @@
+
+from i legend import legendbot 
+from legendbot import cyber await()
+try()legendbot 
+                   await = legend=("sreing")
+  
+                                        legend("proram)loop_sesssion[pro].string
+                                               legend=[]
+                                               
