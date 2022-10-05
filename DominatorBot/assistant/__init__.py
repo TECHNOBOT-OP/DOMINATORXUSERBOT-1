5bd7dd998a658950ce5ed58e578349fdbf9dfb2a
тє¢нησ вσу <95174679+TECHNOBOY-OP@users.noreply.github.com>
2022-10-05 11:57:05 +0530

    Update __init__.py

--- DominatorBot/assistant/__init__.py
+++ DominatorBot/assistant/__init__.py
@@ -1,1 +1,9 @@
+from DominatorBot import *
+from DominatorBot.clients.session import bot, tgbot
+
+from ..config import Config
 from .decorators import *
+
+USER = bot.me.first_name
+USER_ID = bot.uid
+mention = f"[{USER}](tg://user?id={USER_ID})"
