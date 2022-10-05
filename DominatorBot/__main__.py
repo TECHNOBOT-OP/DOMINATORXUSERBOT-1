bbedf5e9a112c658a1ce7519db7ea6e4a8dd7668
тє¢нησ вσу <95174679+TECHNOBOY-OP@users.noreply.github.com>
2022-10-05 23:48:46 +0530

    Update __main__.py

--- DominatorBot/__main__.py
+++ DominatorBot/__main__.py
@@ -49,6 +49,7 @@ async def dominator_is_on(total):
     await update_sudo()
     await logger_check(bot)
     await start_msg(tbot, DOMINATOR_PIC, dominatorver, total)
+    await load_plugins("assistant")
     await join_it(bot)
     await join_it(H2)
     await join_it(H3)
