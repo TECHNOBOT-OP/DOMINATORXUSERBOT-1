19f8a13177006650991d44f1a96247acfd0db759
тє¢нησ вσу <95174679+TECHNOBOY-OP@users.noreply.github.com>
2022-10-06 19:54:40 +0530

    Update startup.py

--- DominatorBot/utils/startup.py
+++ DominatorBot/utils/startup.py
@@ -110,7 +110,7 @@ async def load_plugins(folder, extfolder=None):
             shortname = path1.stem
             pluginname = shortname.replace(".py", "")
             try:
-                if (pluginname not in Config.NO_LOAD) and (
+                if (pluginname not in Config.UNLOAD) and (
                     pluginname not in VPS_NOLOAD
                 ):
                     flag = True
