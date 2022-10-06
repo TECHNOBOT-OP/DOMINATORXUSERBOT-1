370710aa924a4cadbf6a02ef8deff5e57e0d3541
тє¢нησ вσу <95174679+TECHNOBOY-OP@users.noreply.github.com>
2022-10-06 23:08:58 +0530

    Update plug.py

--- DominatorBot/utils/plug.py
+++ DominatorBot/utils/plug.py
@@ -33,28 +33,7 @@ else:
 
 
 # load plugins
-def load_module(shortname, plugin_path=None):
-    if shortname.startswith("__"):
-        pass
-    elif shortname.endswith("_"):
-        import DominatorBot.utils
 
-        path = Path(f"DominatorBot/plugins/{shortname}.py")
-        name = "DominatorBot.plugins.{}".format(shortname)
-        spec = importlib.util.spec_from_file_location(name, path)
-        mod = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(mod)
-        LOGS.info("DominatorBot - Successfully imported " + shortname)
-    else:
-        if plugin_path is None:
-            path = Path(f"DominatorBot/plugins/{shortname}.py")
-            name = f"DominatorBot.plugins.{shortname}"
-        else:
-            path = Path((f"{plugin_path}/{shortname}.py"))
-            name = f"{plugin_path}/{shortname}".replace("/", ".")
-        checkplugins(path)
-        spec = importlib.util.spec_from_file_location(name, path)
-        mod = importlib.util.module_from_spec(spec)
         mod.bot = Dominator
         mod.H1 = Dominator
         mod.H2 = H2
