edf5d0f6b1a433d6bd5d4ca0c6af6ac9c5b802a4
【★༒✞︎©༒SATYA-OP】MR-SATYA-OP <104726886+SATYA-OP@users.noreply.github.com>
2022-10-12 01:23:39 -0700

    Delete DominatorBot/assistant directory

--- DominatorBot/assistant/__init__.py
+++
@@ -1,9 +0,0 @@
-from DominatorBot import *
-from DominatorBot.clients.session import bot, tgbot
-
-from ..config import Config
-from .decorators import *
-
-USER = bot.me.first_name
-USER_ID = bot.uid
-mention = f"[{USER}](tg://user?id={USER_ID})"
